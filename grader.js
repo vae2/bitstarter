d7307fdb9f3b016ae49d323cba91439acfe70d6a
Vincent Emanuele <vincent.emanuele@gmail.com>
2013-07-15 02:50:39 +0000

    Adding in functionality to scan a target url

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -31,13 +33,17 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if (!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting.", instr);
-	process.exit(1)
+	process.exit(1);
     }
     return instr;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioHtmlUrl = function(url) {
+    return cheerio.load(rest.get(url));
 };
 
 var loadChecks = function(checksfile) {
@@ -55,6 +61,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlUrl = function(url, checksfile) {
+    $ = cheerioHtmlUrl(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for (var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue
     // http://stackoverflow.com/a/6772648
@@ -63,13 +80,18 @@ var clone = function(fn) {
 
 if (require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to scan', null)
+	.parse(process.argv);
+    if (program.url === null) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    } else {
+	console.log("Got here")
+	var checkJson = checkHtmlUrl(program.url, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
